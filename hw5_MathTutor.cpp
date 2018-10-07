d90befb34ad45e15c949b836f8ecc997635c7e4c
topher <topherludlow@protonmail.com>
2018-10-06 23:39:40 -0700

    updating save of Math program

--- hw5_MathTutor.cpp
+++ hw5_MathTutor.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <cstlib>
+#include <iomanip>
+#include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
 int main()
 {
-	int Num1, Num2, Num3;
+	int Num1, Num2, Num3, choice, answer;
 
 	srand (time(NULL));
 
@@ -22,3 +23,42 @@ int main()
 	cout << "5. Quit this program              \n";		
 	cout << "--------------------------------- \n";
 	cout << "Enter your choice (1-5): ";
+    cin >> choice;
+
+    if (choice == 1)
+        {
+        Num3=Num1+Num2;
+        cout << "  " << setw(3) << Num1 << endl;
+        cout << "+ " << setw(3) << Num2 << endl;
+        cout << " ---- \n";
+        }
+    else if (choice == 2)
+        {
+        Num3=Num1-Num2;
+        cout << "  " << setw(3) << Num1 << endl;
+        cout << "- " << setw(3) << Num2 << endl;
+        cout << " ---- \n";
+        }
+    else if (choice == 3)
+        {
+        Num3=Num1*Num2;
+        cout << "  " << setw(3) << Num1 << endl;
+        cout << "* " << setw(3) << Num2 << endl;
+        cout << " ---- \n";
+        }
+    else if (choice == 4)
+        {
+        Num3=Num1/Num2;
+        cout << Num1 << " / " << Num2 << " = ";
+        }
+    else if (choice == 5)
+        exit(0);
+
+    cin >> answer;
+
+    if (answer == Num3)
+        cout << "Congratulations! That is correct! \n";
+    else
+        cout << "Sorry, the correct answer is " << Num3 << endl;
+        
+}
